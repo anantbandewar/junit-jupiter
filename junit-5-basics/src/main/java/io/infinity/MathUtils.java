5043b3a3a9eac3fccc3ab65afcce89a113b8c649
Anant Bandewar <anant.bandewar@outlook.com>
2023-05-01 07:50:00 +0530

    1. Project setup + Sample test

---
+++ junit-5-basics/src/main/java/io/infinity/MathUtils.java
@@ -0,0 +1,8 @@
+package io.infinity;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ junit-5-basics/src/test/java/io/infinity/MathUtilsTest.java
@@ -0,0 +1,20 @@
+package io.infinity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MathUtilsTest {
+
+    @Test
+    void add() {
+        MathUtils mathUtils = new MathUtils();
+
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+
+        assertEquals(expected, actual);
+    }
+
+}
