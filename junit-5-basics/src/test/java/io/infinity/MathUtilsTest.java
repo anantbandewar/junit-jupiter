f01470237dddd1ea41eeb9737a7ebcda958bd56e
Anant Bandewar <anant.bandewar@outlook.com>
2023-05-01 08:38:54 +0530

    4. Using Junit lifecycle hook annotations - @BeforeAll, @BeforeEach, @AfterEach, @AfterAll and @TestInstance

--- junit-5-basics/src/test/java/io/infinity/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/infinity/MathUtilsTest.java
@@ -1,16 +1,38 @@
 package io.infinity;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+// This is by DEFAULT. Same as @BeforeEach lifecycle method, the instance of this class gets created.
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+// If we set this, we can have a non-static method for @BeforeAll. As only one instance of this class gets created.
+
 class MathUtilsTest {
+
+    MathUtils mathUtils;
+
+    @BeforeAll
+    // This needs to be static, because we won't be having an instance of this class created at this point.
+    static void beforeAllInit() {
+        System.out.println("This needs to run before all");
+    }
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void cleanup() {
+        System.out.println("Cleaning up...");
+    }
 
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
-
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
@@ -19,8 +41,6 @@ class MathUtilsTest {
 
     @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
-
         double radius = 10;
         double expected = 314.1592653589793;
         double actual = mathUtils.computeCircleArea(radius);
@@ -30,7 +50,6 @@ class MathUtilsTest {
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw ArithmeticException");
     }
 }
